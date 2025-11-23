b385b87587f46d926eb2c03942cc78a56ff4a9b1
Simon <frasselk@gmail.com>
2025-11-23 12:21:20 +0100

    mudblazor

--- Raspberry.Pi.Dashboard/Program.cs
+++ Raspberry.Pi.Dashboard/Program.cs
@@ -1,8 +1,12 @@
+using MudBlazor.Services;
 using Raspberry.Pi.Dashboard.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -17,8 +21,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-// app.UseHttpsRedirection();
+
+//app.UseHttpsRedirection();
+
 
 app.UseAntiforgery();
 
