142b6bd51140226b42453debee3f6555595c97c0
Simon <frasselk@gmail.com>
2025-11-29 22:09:25 +0100

    colorduko first draft

---
+++ Raspberry.Pi.Dashboard/ColordukoExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Raspberry.Pi.Dashboard;
+
+public static class ColorDukoExtensions
+{
+    public static string ToColor(this ColorDukoColors color)
+    {
+        return color switch
+        {
+            ColorDukoColors.Red => "#ff0000",
+            ColorDukoColors.Blue => "#0000ff",
+            ColorDukoColors.Green => "#00ff00",
+            ColorDukoColors.Yellow => "#ffff00",
+            ColorDukoColors.Purple => "#800080",
+            ColorDukoColors.Orange => "#ffA500",
+            ColorDukoColors.Cyan => "#00ffff",
+            ColorDukoColors.Magenta => "#ff00ff",
+            ColorDukoColors.Lime => "#65fe08",
+            _ => ""
+        };
+    }
+}
+
+public enum ColorDukoColors
+{
+    None = 0,
+    Red = 1,
+    Blue = 2,
+    Green = 3,
+    Yellow = 4,
+    Purple = 5,
+    Orange = 6,
+    Cyan = 7,
+    Magenta = 8,
+    Lime = 9
+}
