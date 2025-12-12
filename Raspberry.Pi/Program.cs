5a595ece360f6fdecbd969ae2bae12380dc52a0b
Simon <frasselk@gmail.com>
2025-12-12 12:23:29 +0100

    test 1 sensor

--- Raspberry.Pi/Program.cs
+++ Raspberry.Pi/Program.cs
@@ -1,13 +1,13 @@
 ﻿
 
 var sensor1 = new Vcnl4010(busId: 1);
-var sensor2 = new Vcnl4010(busId: 3);
+// var sensor2 = new Vcnl4010(busId: 3);
 
 while (true)
 {
     int proximity1 = sensor1.GetProximity();
-    int proximity2 = sensor2.GetProximity();
+    // int proximity2 = sensor2.GetProximity();
     Console.WriteLine($"Sensor 1 Proximity: {proximity1}");
-    Console.WriteLine($"Sensor 2 Proximity: {proximity2}");
+    // Console.WriteLine($"Sensor 2 Proximity: {proximity2}");
     Thread.Sleep(1000);
 }
