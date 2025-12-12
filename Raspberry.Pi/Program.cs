8fe2447587683939e0e2ba20bdd452789c2aa6f1
Simon <frasselk@gmail.com>
2025-12-12 14:25:02 +0100

    update

--- Raspberry.Pi/Program.cs
+++ Raspberry.Pi/Program.cs
@@ -22,23 +22,25 @@ while (true)
     int proximity1 = sensor1.GetProximity();
     int proximity2 = sensor2.GetProximity();
 
+    // Segment 1 logic
     if (proximity1 > proximityThreshold && !segment1On)
     {
         await goveeClient.SetSegmentBrightness([0, 1, 2, 3, 4, 5, 6], 100);
         segment1On = true;
     }
-    else if (segment1On)
+    else if (proximity1 <= proximityThreshold && segment1On)
     {
         await goveeClient.SetSegmentBrightness([0, 1, 2, 3, 4, 5, 6], 5);
-        segment2On = false;
+        segment1On = false;  // Fixed this line
     }
 
+    // Segment 2 logic
     if (proximity2 > proximityThreshold && !segment2On)
     {
         await goveeClient.SetSegmentBrightness([7, 8, 9, 10, 11, 12, 13, 14], 100);
         segment2On = true;
     }
-    else if (segment2On)
+    else if (proximity2 <= proximityThreshold && segment2On)
     {
         await goveeClient.SetSegmentBrightness([7, 8, 9, 10, 11, 12, 13, 14], 5);
         segment2On = false;
@@ -46,5 +48,7 @@ while (true)
 
     Console.WriteLine($"Sensor 1 Proximity: {proximity1}");
     Console.WriteLine($"Sensor 2 Proximity: {proximity2}");
-    Thread.Sleep(1000);
+
+    await Task.Delay(1000); // Use async delay
 }
+
