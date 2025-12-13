513ca116e117cb83c948dbc26d2d84edc87096f7
Simon <frasselk@gmail.com>
2025-12-13 20:00:44 +0100

    state

--- Raspberry.Pi.Dashboard/ProximityEventHandler.cs
+++ Raspberry.Pi.Dashboard/ProximityEventHandler.cs
@@ -4,14 +4,19 @@ namespace Raspberry.Pi.Dashboard;
 public class ProximityEventHandler
 {
     private readonly GoveeClient _goveeClient;
-    public ProximityEventHandler(ProximitySensorReaderBackgroundService reader, GoveeClient goveeClient)
+    private readonly ProximityUiState _state;
+
+    public ProximityEventHandler(ProximitySensorReaderBackgroundService reader, GoveeClient goveeClient, ProximityUiState state)
     {
         reader.ProximityThresholdReached += OnThresholdReached;
         _goveeClient = goveeClient;
+        _state = state;
     }
 
     private async void OnThresholdReached(object? sender, ProximityEvent e)
     {
+        _state.Update(e);
+
         // algorithm
         if (e.Sensor == Sensor.Sensor1)
         {
